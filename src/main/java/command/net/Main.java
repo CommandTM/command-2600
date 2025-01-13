99fa8d9d117925370148ac1981ebad01aa1aa7b5
CommandTM <commandnoobmc23@gmail.com>
2025-01-13 10:02:22 -0500

    Readme Fix

--- src/main/java/command/net/Main.java
+++ src/main/java/command/net/Main.java
@@ -3,7 +3,7 @@ package command.net;
 public class Main {
     public static void main(String[] args) {
         try {
-            Atari atari = new Atari("C:\\Users\\ce3300\\Downloads\\ConJumpTest.bin", true);
+            Atari atari = new Atari("C:\\Users\\ce3300\\Downloads\\Inaccuracy2.bin", false);
             atari.run();
         } catch (Exception e) {
             e.printStackTrace();
